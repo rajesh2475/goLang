8eb93f55fcbf3e8c2ded8df21b7b8e0fee0b1a89
rajesh2475 <rajesh2475@users.noreply.github.com>
2021-07-19 23:31:39 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -2,55 +2,31 @@ package main
 
 import "fmt"
 
-type address struct {
-	address1 string
-	address2 string
-	zipCode  int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	phoneNum  string
-	age       int
-	address   // address => address(vName) address(Type)
-}
-
 func main() {
-	// Method 1
-	// details := person{"Rajesh", "M", "7760832839", 26}
-
-	// Method 2
-	details := person{
-		firstName: "Rajesh",
-		lastName:  "M",
-		phoneNum:  "7760832839",
-		age:       26,
-		address: address{
-			address1: "Banaswadi",
-			address2: "bangalore",
-			zipCode:  560033,
-		},
+	values := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, value := range values {
+		if value%2 == 0 {
+			fmt.Println(fmt.Sprintf("The value : %d is even", value))
+		} else {
+			fmt.Println(fmt.Sprintf("The value : %d is odd", value))
+		}
 	}
-
-	// Method 3
-	// var details person
-	// details.age = 26
-	// details.firstName = "Rajesh"
-	// details.lastName = "M"
-	// details.phoneNum = "7760832839"
-	// pointerAddress := &details
-	// pointerAddress.update("Deepu")
-	details.update("Deepu")
-	details.printDetails()
-
 }
 
-func (p person) printDetails() {
-	fmt.Printf("%+v", p)
-	fmt.Println()
-}
+// func main() {
+// 	cards := newDeck()
+// 	// cards.print()
+// 	// cardsHand, remainingCards := cards.deal(5)
+// 	// cardsHand.print()
+// 	// remainingCards.print()
 
-func (p *person) update(firstName string) {
-	p.firstName = firstName
-}
+// 	bytesData := toBytesArray(cards.toString())
+// 	// fmt.Println(string([]byte(strings.Join(cards, ","))))
+// 	writeToFile(bytesData)
+// 	stringData := readFromFile()
+// 	finalResult := stringToDeck(stringData)
+// 	finalResult.print()
+// 	// fmt.Println("1111111111111111111111111111111111111111111111")
+// 	finalResult.shuffel().print()
+
+// }
